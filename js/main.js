e3506863e14622fcd32665b2f8f5bc7f56be4c67
leoalevalle2014 <leoalevalle2014@gmail.com>
2026-04-12 19:38:17 -0300

    creacion de Home.html

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const toggleBtn = document.getElementById('modo-toggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  toggleBtn.textContent = document.body.classList.contains('dark-mode')
+    ? '☀️'
+    : '🌙';
+});
+
+  const counters = document.querySelectorAll('.counter');
+
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const current = +counter.innerText;
+      const increment = target / 100;
+
+      if (current < target) {
+        counter.innerText = Math.ceil(current + increment);
+        setTimeout(updateCount, 30);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    // Activar después de que aparezcan
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          updateCount();
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 0.5 });
+
+    observer.observe(counter);
+  });
