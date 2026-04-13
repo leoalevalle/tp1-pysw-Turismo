ae0f9b1948f5f7cfa0cc95dc746c076863b347b5
leoalevalle2014 <leoalevalle2014@gmail.com>
2026-04-13 01:11:03 -0300

    implementacion modo oscuro

--- js/home.js
+++ js/home.js
@@ -5,32 +5,3 @@ toggleBtn.addEventListener('click', () => {
     ? '☀️'
     : '🌙';
 });
-
-  const counters = document.querySelectorAll('.counter');
-
-  counters.forEach(counter => {
-    const updateCount = () => {
-      const target = +counter.getAttribute('data-target');
-      const current = +counter.innerText;
-      const increment = target / 100;
-
-      if (current < target) {
-        counter.innerText = Math.ceil(current + increment);
-        setTimeout(updateCount, 30);
-      } else {
-        counter.innerText = target;
-      }
-    };
-
-    // Activar después de que aparezcan
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          updateCount();
-          observer.unobserve(entry.target);
-        }
-      });
-    }, { threshold: 0.5 });
-
-    observer.observe(counter);
-  });
